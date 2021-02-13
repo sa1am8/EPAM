bdb75d6c4bd63d3caf9d91fe02c77996c26837f2
Anton Saltykov <tinkerino571@gmail.com>
2021-02-13 08:34:26 +0200

    1

--- models/models.py
+++ models/models.py
@@ -46,6 +46,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    grades = db.Column(db.PickleType)
 
     def __repr__(self):
         return '<Name: {}>'.format(self.name)
--- models/profile.py
+++ models/profile.py
@@ -2,10 +2,11 @@ from flask import render_template, Blueprint, url_for, redirect, request, flash
 from app import db
 from flask_login import login_required, current_user
 
+
 prf = Blueprint("profile", __name__)
 
 
 @prf.route('/profile')
 @login_required
 def profile():
-    return render_template('html/profile.html', name=current_user.name)
+    return render_template('html/profile.html', current_user=current_user, name=current_user.name, id=current_user.id)
